b7c906f1fbadeb5dc7c20fcf23f27a919537115a
Walt Woods <waltw@galois.com>
2021-09-07 12:11:32 -0700

    Added base entity/observation model mermaid plot

--- lagoon/ingest/__init__.py
+++ lagoon/ingest/__init__.py
@@ -4,5 +4,23 @@ to LAGOON.
 TODO NOTE -- Each ingest module should generate a unique ID for each operation;
 incremental updates / clearing out previously loaded data is the burden of each
 module on its own!
+
+Entity/Observation model
+------------------------
+
+Spatiotemporal graphs within LAGOON are represented via an entity/observation
+model. This model implies that there are certain, time-invariant entities (nouns)
+which interact with one another over some number of observations. Observations
+are directional by default, and occur between two entities. Both entities and
+observations may have additional attributes attached to them, the significance
+of which is specific to the type of entity or observation.
+
+.. mermaid::
+
+    flowchart RL
+    entity["entity, first type<br/><div style='text-align:left'>+attribute1<br/>+attribute2</div>"]
+    entity -- "observation, type<br/>+attributes" --> entity
+    entity2["entity, second type"]
+    entity2 -- "observation, other type" --> entity
 """
 
